84f08f0d5624a713b557e797059950ce588c5d4c
smiledpeace <863899253@qq.com>
2022-01-27 10:00:37 +0800

    feat: async error cathc

--- libs/utils.js
+++ libs/utils.js
@@ -1,25 +1,24 @@
+const logger = require('../libs/log')('async-pool')
+
 async function asyncPool (poolLimit, array, iteratorFn) {
   const ret = []; // 存储所有的异步任务
   const executing = []; // 存储正在执行的异步任务
   
-  for (const [index, item] of array.entries()) {
+  for (const item of array) {
     // 调用iteratorFn函数创建异步任务
-    const p = Promise.resolve().then(() => iteratorFn(item, array));
+    const p = Promise.resolve().then(() => iteratorFn(item, array)).catch(e => {
+      logger.error(e)
+    });
     ret.push(p); // 保存新的异步任务
-    console.log(index)
     // 当poolLimit值小于或等于总任务个数时，进行并发控制
     if (poolLimit <= array.length) {
+      // console.log(index)
       // 当任务完成后，从正在执行的任务数组中移除已完成的任务
       const e = p.then(() => executing.splice(executing.indexOf(e), 1));
+      // console.log(executing)
       executing.push(e); // 保存正在执行的异步任务
       if (executing.length >= poolLimit) {
-        try {
-          await Promise.race(executing); // 等待较快的任务执行完成
-        } catch (e) {
-          // console.log(executing.indexOf(e))
-          ret.splice(index - 1, 1)
-          break
-        }
+        await Promise.race(executing); // 等待较快的任务执行完成
       }
     }
   }
--- services/UploadSDK.js
+++ services/UploadSDK.js
@@ -14,8 +14,8 @@ const logger = require('../libs/log')('sdk')
 const fetch = function (url) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      console.log(url)
-      if (url === 'G:\\leader-node\\data\\testSync\\2.js') {
+      console.log(1)
+      if (url === 'G:\\leader-node\\data\\testSync\\3.js') {
         reject('get file error ' + url)
       } else {
         resolve(url)
@@ -27,9 +27,7 @@ const fetch = function (url) {
 
 module.exports = function run () {
   const promise = getFile(path.join(__dirname, '..' + config.dirs[0].local))
-  console.log(promise)
   asyncPool( 2, promise, fetch).then(res => {
-    console.log(2123)
     console.log(res)
   })
   
